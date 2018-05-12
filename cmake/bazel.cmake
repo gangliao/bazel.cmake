dcbb57a01384a8a5bcc7e6c1cc1b004947e576f6
liaogang <gangliao@gatech.edu>
2018-05-12 11:30:01 +0800

    Fix third party

--- cmake/bazel.cmake
+++ cmake/bazel.cmake
@@ -29,7 +29,8 @@ endif(NOT CMAKE_CROSSCOMPILING)
 option(WITH_GPU     "Compile Source Code with NVIDIA GPU"     ${CUDA_FOUND})
 option(WITH_TESTING "Compile Source Code with Unit Testing"   OFF)
 
-set(BAZEL_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR})
+get_filename_component(BAZEL_THIRD_PARTY_DIR ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
+set(BAZEL_THIRD_PARTY_DIR ${BAZEL_THIRD_PARTY_DIR}/third-party)
 
 include(merge_libs)
 include(external/cuda)
