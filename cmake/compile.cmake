aedc24ff467d69be7812eb9bfa20c05cd9b9e073
liaogang <gangliao@gatech.edu>
2018-05-14 16:27:39 +0800

    fix cuda_arch flag function detected

--- cmake/compile.cmake
+++ cmake/compile.cmake
@@ -56,5 +56,55 @@ else(MSVC)
     set(CMAKE_CXX_FLAGS "-Wall -std=c++11 -fPIC")
 endif(MSVC)
 
+
+################################################################################################
+# A function for automatic detection of GPUs installed  (if autodetection is enabled)
+# Usage:
+#   detect_installed_gpus(out_variable)
+function(detect_installed_gpus out_variable)
+    if(NOT CUDA_gpu_detect_output)
+        set(__cufile ${PROJECT_BINARY_DIR}/detect_cuda_archs.cu)
+
+        file(WRITE ${__cufile} ""
+        "#include <cstdio>\n"
+        "int main()\n"
+        "{\n"
+        "  int count = 0;\n"
+        "  if (cudaSuccess != cudaGetDeviceCount(&count)) return -1;\n"
+        "  if (count == 0) return -1;\n"
+        "  for (int device = 0; device < count; ++device)\n"
+        "  {\n"
+        "    cudaDeviceProp prop;\n"
+        "    if (cudaSuccess == cudaGetDeviceProperties(&prop, device))\n"
+        "      std::printf(\"%d.%d \", prop.major, prop.minor);\n"
+        "  }\n"
+        "  return 0;\n"
+        "}\n")
+        enable_language(CUDA)
+
+        try_run(__nvcc_res __compile_result ${PROJECT_BINARY_DIR} ${__cufile}
+        COMPILE_OUTPUT_VARIABLE __compile_out
+        RUN_OUTPUT_VARIABLE __nvcc_out)
+
+        if(__nvcc_res EQUAL 0 AND __compile_result)
+            # nvcc outputs text containing line breaks when building with MSVC.
+            # The line below prevents CMake from inserting a variable with line
+            # breaks in the cache
+            string(REGEX MATCH "([1-9].[0-9])" __nvcc_out "${__nvcc_out}")
+            string(REPLACE "2.1" "2.1(2.0)" __nvcc_out "${__nvcc_out}")
+            set(CUDA_gpu_detect_output ${__nvcc_out})
+        else()
+            message(WARNING "Running GPU detection script with nvcc failed: ${__nvcc_out} ${__compile_out}")
+        endif()
+    endif()
+
+    if(NOT CUDA_gpu_detect_output)
+        message(WARNING "Automatic GPU detection failed. Building for the default architectures.")
+        set(${out_variable} "" PARENT_SCOPE)
+    else()
+        set(${out_variable} ${CUDA_gpu_detect_output} PARENT_SCOPE)
+    endif()
+endfunction()
+
 detect_installed_gpus(CUDA_NVCC_ARCH_FLAGS)
 list(APPEND CUDA_NVCC_FLAGS ${CUDA_NVCC_ARCH_FLAGS} -Wno-deprecated-gpu-targets -std=c++11)
